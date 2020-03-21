aa59a8dea078aea45020a9a84a87ec3742dfd0b9
aland1013 <adam.land@gmail.com>
2020-03-20 21:26:17 -0400

    coded Tabs/index.js

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,19 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const response = axios.get('https://lambda-times-backend.herokuapp.com/topics');
+
+console.log('response', response);
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+  .then(response => {
+    response.data.topics.forEach(item => {
+      const tab = document.createElement('div');
+  
+      tab.classList.add('tab');
+      tab.textContent = `${item}`;
+
+      document.querySelector('.topics').appendChild(tab);
+    })
+  });
